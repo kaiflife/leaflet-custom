d1db6ca175703d2e6457900ba7dd5faae13289de
Pavel Petrunkin <makesomecode@gmail.com>
2022-07-17 13:14:48 +0300

    added Map class, added tracked marker functions with example

---
+++ CustomMap.js
@@ -0,0 +1,114 @@
+import { BaseMarker } from './BaseMarker';
+
+export default class CustomMap {
+  constructor({ mapId, L }) {
+    this.map = this.createMap({ id: mapId });
+    this.L = L;
+  }
+
+  // MAP
+  //
+  createTileLayer(map) {
+    var tileUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      layer = new this.L.TileLayer(tileUrl,
+        {
+          attribution: 'Maps © <a href=\"www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors',
+          maxZoom: 18,
+        });
+
+    map.addLayer(layer);
+  }
+
+  createMap({ id = 'map' }) {
+    var map = new this.L.Map(
+      id, {
+        zoom: 6,
+        minZoom: 3,
+      });
+
+    this.createTileLayer(map);
+
+    return map;
+  }
+
+
+  // MARKER
+  //
+  computeBearing(previousPosition = [0, 0], nexPosition) {
+    let bearing = Math.atan2(nexPosition[0] - previousPosition[0], nexPosition[1] - previousPosition[1])
+    bearing = bearing * (180 / Math.PI)
+    bearing = (bearing + 360) % 360
+    bearing = 360 - bearing
+    return bearing
+  }
+
+  createMarker({ position, previousPosition, rotationAngle, ...options }) {
+    const bearingAngle = rotationAngle ?? this.computeBearing(previousPosition, position)
+    const instance = new BaseMarker(position, { ...options, bearingAngle })
+    return { instance, context: { ...this.L.BaseMarker, overlayContainer: instance } }
+  }
+
+  createMarkerOnMap(createMarkerProps, map) {
+    const newMarker = this.createMarker(createMarkerProps);
+    newMarker.instance.addTo(map);
+    return newMarker;
+  }
+
+  updateMarker(marker, props, prevProps) {
+    const { position, previousPosition, duration, keepAtCenter, icon, zIndexOffset, opacity, draggable, rotationOrigin, rotationAngle } = props
+    if (prevProps.position !== position && typeof duration == 'number') {
+      marker.slideTo(position, {
+        duration,
+        keepAtCenter
+      })
+    }
+    if (icon && icon !== prevProps.icon) {
+      marker.setIcon(icon)
+    }
+    if (zIndexOffset && zIndexOffset !== prevProps.zIndexOffset) {
+      marker.setZIndexOffset(zIndexOffset)
+    }
+    if (opacity && opacity !== prevProps.opacity) {
+      marker.setOpacity(opacity)
+    }
+    if (marker.dragging && draggable !== prevProps.draggable) {
+      if (draggable === true) {
+        marker.dragging.enable()
+      } else {
+        marker.dragging.disable()
+      }
+    }
+    if (rotationAngle) {
+      marker.setRotationAngle(rotationAngle)
+    } else if (previousPosition?.[0] !== position[0] && previousPosition?.[1] !== position[1]) {
+      const bearingAngle = this.computeBearing(previousPosition, position)
+      marker.setRotationAngle(bearingAngle)
+    }
+
+    if (rotationOrigin !== prevProps.rotationOrigin) {
+      marker.setRotationOrigin(rotationOrigin)
+    }
+  }
+
+  // Marker Cluster Group
+  //
+  addClusterGroup({
+    map,
+    coordinates = [],
+    chunkedLoading = true,
+    polygonOptions = {
+      stroke: true,
+      color: 'red',
+      weight: 4,
+      opacity: 0.5,
+      fill: false,
+      clickable: true,
+    },
+  }) {
+    var markers = this.L.markerClusterGroup({ polygonOptions, chunkedLoading });
+    coordinates.forEach(function (latLng) {
+      markers.addLayer(this.L.marker(latLng));
+    });
+    map.addLayer(markers);
+  }
+}
--- index.js
+++ index.js
@@ -1,44 +1,21 @@
-var londonParisRomeBerlinBucarest = [[51.507222, -0.1275], [48.8567, 2.3508],
-  [41.9, 12.5], [52.516667, 13.383333], [44.4166, 26.1]];
+import CustomMap from './CustomMap';
+import L from 'leaflet';
 
-export default function createMap({ id = 'map' }) {
-  var map = new L.Map(
-    id, {
-      zoom: 6,
-      minZoom: 3,
-    });
+const mapInstance = new CustomMap({ mapId: 'map', L });
 
-  createTileLayer(map);
-}
-
-function createTileLayer(map) {
-  var tileUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-    layer = new L.TileLayer(tileUrl,
-      {
-        attribution: 'Maps © <a href=\"www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors',
-        maxZoom: 18,
-      });
-
-  map.addLayer(layer);
-}
-
-export function moveMarker({
-  coordinates = londonParisRomeBerlinBucarest,
-  generalDuration = 3000,
-  durations = []
-}) {
-  var mainDurations = [generalDuration];
-
-  if (durations.length) {
-    mainDurations = durations;
-  }
-
-  map.fitBounds(coordinates);
-
-  var movingMarker = L.Marker.movingMarker(coordinates, mainDurations, { autostart: true }).addTo(map);
-
-  movingMarker.on('end', function () {
-    movingMarker.bindPopup('<b>Welcome to Bucarest !</b>', { closeOnClick: false })
-      .openPopup();
+export function exampleCreateMarkerOnMap() {
+  const { instance: markerInstance } = mapInstance.map.createMarkerOnMap({
+    position: [55.22376666666667, 50.745841666666664],
+    previousPosition: [53.22376666666667, 50.745841666666664],
+    duration: 1000,
+    icon,
   });
+
+  setTimeout(() => mapInstance.map.updateMarker(
+    markerInstance,
+    { position: [53.22376666666667, 50.745841666666664], duration: 1000 },
+    { position: [55, 50.74598666666667] },
+  ), 1000);
 }
+
+exampleCreateMarkerOnMap();
